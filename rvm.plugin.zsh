001ccf1f88b06e87019f351b7ed479a4ac6437ae
Pradyun <pradyunsg@users.noreply.github.com>
2016-12-04 10:31:34 +0530

    Add completions support
    Also, reformat comments

--- rvm.plugin.zsh
+++ rvm.plugin.zsh
@@ -1,2 +1,11 @@
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" ]] # Load RVM into a shell session *as a function*
+# Add RVM to PATH for scripting
+PATH=$PATH:$HOME/.rvm/bin
+
+# Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" ]]
+
+# Enable rvm completions
+function _rvm_completion {
+  source $rvm_path"/scripts/zsh/Completion/_rvm"
+}
+compdef _rvm_completion rvm
